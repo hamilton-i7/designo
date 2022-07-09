d4818d5d3ff455a4b0863cd7c4ceec889aeccda7
Juan Hamilton <hedwards507@gmail.com>
2022-07-09 17:34:18 -0500

    Create Designo theme

---
+++ frontend/styles/globals.js
@@ -0,0 +1,69 @@
+export const globalStyles = {
+  html: { fontSize: '62.5%' },
+  img: { display: 'block', width: '100%' },
+  a: { color: 'currentColor', textDecoration: 'none' },
+}
+
+export const breakpoints = {
+  values: {
+    xs: 0,
+    sm: 600,
+    tablet: 768,
+    md: 900,
+    lg: 1200,
+    desktop: 1440,
+    xl: 1536,
+  },
+}
+
+export const typography = {
+  fontFamily: ['Jost', 'Arial', 'sans-serif'].join(','),
+  h1: {
+    fontSize: '3.2rem',
+    fontWeight: 500,
+    lineHeight: '3.6rem',
+  },
+  h2: {
+    fontSize: '2.8rem',
+    fontWeight: 500,
+    letterSpacing: '0.14rem',
+    lineHeight: '3.6rem',
+  },
+  h3: {
+    fontSize: '2rem',
+    fontWeight: 500,
+    lineHeight: '2.6rem',
+  },
+  body1: {
+    fontWeight: 400,
+    fontSize: '1.6rem',
+    lineHeight: '2.6rem',
+  },
+  subtitle1: {
+    fontSize: '2.4rem',
+    fontWeight: 400,
+    letterSpacing: '0.2rem',
+    lineHeight: '2.5rem',
+  },
+  subtitle2: {
+    fontSize: '1.4rem',
+    fontWeight: 400,
+    letterSpacing: '0.2rem',
+    lineHeight: '1.4rem',
+  },
+  button: {
+    fontSize: '1.5rem',
+    fontWeight: 500,
+    letterSpacing: '0.1rem',
+  },
+  caption: {
+    fontSize: '1.5rem',
+    fontWeight: 500,
+    lineHeight: '2.6rem',
+  },
+  overline: {
+    fontSize: '1.2rem',
+    fontWeight: '400',
+    fontStyle: 'italic',
+  },
+}
---
+++ frontend/styles/theme/lightTheme.js
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material'
+import { breakpoints, typography } from '../globals'
+
+const lightTheme = createTheme({
+  breakpoints,
+  spacing: factor => `${0.8 * factor}rem`,
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#E7816B',
+    },
+    secondary: {
+      main: '#FFAD9B',
+    },
+    common: {
+      black: '#1D1C1E',
+      darkGrey: '#333136',
+      lightGray: '#F1F3F5',
+    },
+  },
+  typography,
+})
+
+export default lightTheme
