751379e6f4a0a3398d567fb1e45fade34209d6e2
Juan Hamilton <hedwards507@gmail.com>
2022-07-11 20:19:37 -0500

    Add styles for the header

--- frontend/components/hero/heroWithImage.js
+++ frontend/components/hero/heroWithImage.js
@@ -9,10 +9,29 @@ const HeroWithImage = ({ hero }) => {
   const pattern = getStrapiMedia(hero.pattern)
 
   return (
-    <Stack component='header'>
+    <Stack
+      component='header'
+      sx={{
+        background: `no-repeat url(${pattern.url})`,
+        backgroundSize: '85rem',
+        backgroundPosition: '100% -95%',
+        backgroundColor: theme => theme.palette.primary.main,
+        color: theme => theme.palette.common.white,
+        textAlign: 'center',
+      }}>
       <Box component='img' src={image.url} alt={image.alternativeText} />
-      <Box>
-        <Typography variant='h1'>{hero.title}</Typography>
+      <Box
+        sx={{
+          m: theme => theme.spacing(10, 3),
+        }}>
+        <Typography
+          variant='h1'
+          textTransform='capitalize'
+          sx={{
+            mb: theme => theme.spacing(3),
+          }}>
+          {hero.title}
+        </Typography>
         <Typography variant='body2'>{hero.description}</Typography>
       </Box>
     </Stack>
