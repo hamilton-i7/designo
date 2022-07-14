ee9bf2d6436620ee6d6fec82d3ffffc84b1df43f
Juan Hamilton <hedwards507@gmail.com>
2022-07-14 16:09:57 -0500

    Add loading spinner

--- frontend/components/map.js
+++ frontend/components/map.js
@@ -5,6 +5,7 @@ import {
   useSmallScreenMatcher,
 } from '../lib/responsive'
 import { useTheme } from '@mui/material'
+import Loader from './loader'
 
 const Map = ({ center }) => {
   const theme = useTheme()
@@ -28,13 +29,13 @@ const Map = ({ center }) => {
 
 const DesignoMap = ({ lat, lng }) => {
   const center = useMemo(() => ({ lat, lng }), [])
+  const theme = useTheme()
   // TODO: Restrict API access
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   })
 
-  // TODO: Add loading Spinner
-  if (!isLoaded) return <div>Loading...</div>
+  if (!isLoaded) return <Loader show={isLoaded} />
   return <Map center={center} />
 }
 
