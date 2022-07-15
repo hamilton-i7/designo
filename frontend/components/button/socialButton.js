bad9363c6a9aeb454bcb5a5de72eff070ca05084
Juan Hamilton <hedwards507@gmail.com>
2022-07-15 00:10:31 -0500

    Add babel parser

--- frontend/components/button/socialButton.js
+++ frontend/components/button/socialButton.js
@@ -3,7 +3,7 @@ import FacebookIcon from '@mui/icons-material/Facebook'
 import TwitterIcon from '@mui/icons-material/Twitter'
 import InstagramIcon from '@mui/icons-material/Instagram'
 import PinterestIcon from '@mui/icons-material/Pinterest'
-import YoutubeIcon from '@mui/icons-material/Youtube'
+import YoutubeIcon from '@mui/icons-material/YouTube'
 import IconButton from '@mui/material/IconButton'
 
 const SocialButton = ({ socialMedia, url, sx }) => {
