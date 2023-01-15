c008a53829db7cb0b57bc5a9ed49693d35b428f7
Juan Hamilton <hedwards507@gmail.com>
2023-01-15 11:29:27 -0500

    Remove unused imports

--- components/designs.js
+++ components/designs.js
@@ -4,7 +4,6 @@ import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import { useStrapiMedia } from '../lib/media'
 import { MAX_WIDTH } from '../lib/responsive'
 
 const Designs = ({ designs, sx }) => {
